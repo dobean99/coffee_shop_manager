12150f6ea2c9dd58dccdb44238194cd84ac4f07d
dkndo <dnkdo@tma.com.vn>
2026-03-27 10:46:34 +0700

    Initial Commit

---
+++ CoffeeShopManager/CoffeeShopManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopManagerApp.swift
+//  CoffeeShopManager
+//
+//  Created by dnkdo on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
